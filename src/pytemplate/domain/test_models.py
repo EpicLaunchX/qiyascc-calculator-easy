9c08c06bc82356e8318f335aa4024b12d78907d4
Qiyas <107604130+qiyascc@users.noreply.github.com>
2024-04-30 01:00:47 +0400

    Test

---
+++ src/pytemplate/domain/test_models.py
@@ -0,0 +1,11 @@
+import unittest
+from pytemplate.domain.models import Operands
+
+class TestOperands(unittest.TestCase):
+    def test_operands_creation(self):
+        operands = Operands(5, 10)
+        self.assertEqual(operands.first_operand, 5)
+        self.assertEqual(operands.second_operand, 10)
+
+if __name__ == '__main__':
+    unittest.main()
